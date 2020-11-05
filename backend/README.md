c15e81f86f8ea1c7058e2b44b76abf5f60da332f
ericrkuo <ericrkuo@gmail.com>
2020-11-04 22:53:46 -0800

    Added more sample data using Yu's data

--- backend/README.md
+++ backend/README.md
@@ -1,1 +1,18 @@
 ## Foodies Backend Project
+
+Requirements
+1. Postgres
+2. TypeORM Global Installation (`yarn add typeorm -g`)
+    - this will install the TypeORM CLI as well
+
+##### To import data from `init_data.sql`, run this command inside a command prompt. Make sure the schema is setup and it is currently empty
+
+> `psql -d foodies -f resources/init_data.sql --username={YOUR_USERNAME}`
+>
+> `psql` should prompt you to enter your password.
+
+##### To drop the database schema, run:
+>`yarn run dropSchema`
+
+##### To sync the database schema, run: (this will setup the schema, without any data)
+ >`yarn run syncSchema`
--- backend/src/App.ts
+++ backend/src/App.ts
@@ -4,9 +4,7 @@ import bodyParser from "body-parser";
 import {User} from "./entity/User";
 import {Promotion} from "./entity/Promotion";
 import {Discount} from "./entity/Discount";
-import {DiscountType} from "./data/DiscountType";
-import {PromotionCategory} from "./data/PromotionCategory";
-import {CuisineType} from "./data/CuisineType";
+import {promotions_sample, saved_promotions_mapping, users_sample} from "./resources/Data";
 
 //todo: ormconfig.json should not have synchronize and drop schema as true for production
 createConnection()
@@ -15,35 +13,24 @@ createConnection()
         app.use(bodyParser.json())
         const PORT = 8000;
 
-        // creating sample data
-        // todo: refactor to make cleaner
-        const discount1 = new Discount(DiscountType.PERCENTAGE, 50.5);
-        const discount2 = new Discount(DiscountType.AMOUNT, 2);
-        const discount3 = new Discount(DiscountType.AMOUNT, 3);
-        const discount4 = new Discount(DiscountType.PERCENTAGE, 19.99);
-
-        const user1 = new User("user1", "test", "user1Email", "user1_userName", "user1_password");
-        const user2 = new User("user2", "test", "user2Email", "user2_userName", "user2_password");
-
-        const promotion1 = new Promotion(user1, [discount3], PromotionCategory.BOGO, CuisineType.CARIBBEAN, "promo1", "test", new Date(), "$")
-        const promotion2 = new Promotion(user2, [discount1, discount2], PromotionCategory.HAPPY_HOUR, CuisineType.JAPANESE, "promo2", "test", new Date(), "$")
-        const promotion3 = new Promotion(user1, [discount4], PromotionCategory.BOGO, CuisineType.KOREAN, "promo3", "test", new Date(), "$")
-
         // todo: create custom repositories
         let userRepository: Repository<User> = getRepository(User);
         let promotionRepository: Repository<Promotion> = getRepository(Promotion);
         let discountRepository: Repository<Discount> = getRepository(Discount);
 
         // persist entities into database
-        await userRepository.save(user1);
-        await userRepository.save(user2);
-        await promotionRepository.save(promotion1);
-        await promotionRepository.save(promotion2);
-        await promotionRepository.save(promotion3);
+        for (let user of users_sample) {
+            await userRepository.save(user);
+        }
 
-        // if user1 wants to add promotion to savedPromotions, this works because of { cascade: true } in User,
-        user1.savedPromotions = [promotion1];
-        await userRepository.save(user1);
+        for (let promotion of promotions_sample) {
+            await promotionRepository.save(promotion)
+        }
+
+        for (let [user, promotions] of saved_promotions_mapping) {
+            user.savedPromotions = promotions;
+            await userRepository.save(user);
+        }
 
         // eager loading (loads everything)
         let users: User[] = await userRepository.find({relations: ["uploadedPromotions", "savedPromotions"]});
