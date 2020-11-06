3769539d4786df6ed62c3a7c4170cd7a4b93f390
Andrew Zulaybar <andrewzulaybar@gmail.com>
2020-11-06 00:15:01 -0800

    create promotion card components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,49 @@
 import React, { ReactElement } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+
+import PromotionCard from "./components/promotion/PromotionCard";
+import { Promotion } from "./types/Promotion";
+
+const promotions: Promotion[] = [
+  {
+    title: "Happy Hour 2pm-4pm",
+    restaurantName: "Starbucks",
+    description: "Buy one, get one free on all Grande sized drinks!",
+    date: "Nov 11, 2020",
+    liked: false,
+    image: { src: "" },
+  },
+  {
+    title: "$2 off Sandwiches",
+    restaurantName: "Grandma Loves You",
+    description: "Get $2 off any sandwich of your choice.",
+    date: "Nov 20, 2020",
+    liked: false,
+    image: { src: "" },
+  },
+  {
+    title: "10% off Breakfast",
+    restaurantName: "Elephant Grind Coffee House",
+    description:
+      "Get 10% off of your order (pre-tax) when you spend over $15 on breakfast.",
+    date: "Thursday",
+    liked: true,
+    image: { src: "" },
+  },
+];
 
 function App(): ReactElement {
+  const mapWidth = 60;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ display: "inline-flex" }}>
+        <div style={{ width: `${mapWidth}%` }}></div>
+        <div style={{ width: `${100 - mapWidth}%` }}>
+          {promotions.map((promotion: Promotion) => (
+            <PromotionCard {...promotion} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ frontend/src/types/Promotion.ts
@@ -0,0 +1,12 @@
+export interface Promotion {
+  title: string;
+  restaurantName?: string;
+  description?: string;
+  date?: string;
+  liked?: boolean;
+  image: PromotionImage;
+}
+
+export interface PromotionImage {
+  src: string;
+}
