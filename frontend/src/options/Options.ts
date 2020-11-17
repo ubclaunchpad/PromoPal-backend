660e32588ad3fba95c5709a8f15762711c0156c7
richardyjtian <richardyjtian@hotmail.com>
2020-11-16 19:56:36 -0800

    Added options and theme objects

---
+++ frontend/src/options/Options.ts
@@ -0,0 +1,147 @@
+import { Dropdown } from "../types/Dropdown";
+
+export const SortBy: Dropdown = {
+  text: "Sort By",
+  options: [
+    {
+      link: "/",
+      text: "Distance"
+    },
+    {
+      link: "/",
+      text: "Most Popular"
+    },
+    {
+      link: "/",
+      text: "Most Recently Uploaded"
+    },
+    {
+      link: "/",
+      text: "Most Recently Favourited"
+    }
+  ]
+}
+
+export const DiscountType: Dropdown = {
+  text: "Discount Type",
+  options: [
+    {
+      link: "/",
+      text: "% Off"
+    },
+    {
+      link: "/",
+      text: "$ Off"
+    },
+    {
+      link: "/",
+      text: "Happy Hour"
+    },
+    {
+      link: "/",
+      text: "Buy One Get One"
+    }
+  ]
+}
+
+export const Cuisine: Dropdown = {
+  text: "Cuisine",
+  options: [
+    {
+      link: "/",
+      text: "American"
+    },
+    {
+      link: "/",
+      text: "Chinese"
+    },
+    {
+      link: "/",
+      text: "Mexican"
+    },
+    {
+      link: "/",
+      text: "Italian"
+    },
+    {
+      link: "/",
+      text: "Japanese"
+    },
+    {
+      link: "/",
+      text: "Greek"
+    },
+    {
+      link: "/",
+      text: "French"
+    },
+    {
+      link: "/",
+      text: "Thai"
+    },
+    {
+      link: "/",
+      text: "Spanish"
+    },
+    {
+      link: "/",
+      text: "Indian"
+    },
+    {
+      link: "/",
+      text: "Mediterranean"
+    },
+    {
+      link: "/",
+      text: "Vietnamese"
+    },
+  ]
+}
+
+export const DayOfTheWeek: Dropdown = {
+  text: "Day Of The Week",
+  options: [
+    {
+      link: "/",
+      text: "All Week"
+    },
+    {
+      link: "/",
+      text: "Monday"
+    },
+    {
+      link: "/",
+      text: "Tuesday"
+    },
+    {
+      link: "/",
+      text: "Wednesday"
+    },
+    {
+      link: "/",
+      text: "Thursday"
+    },
+    {
+      link: "/",
+      text: "Friday"
+    },
+    {
+      link: "/",
+      text: "Saturday"
+    },
+  ]
+}
+
+export const ServiceOptions: Dropdown = {
+  text: "Service Options",
+  options: [
+    {
+      link: "/",
+      text: "Dine In"
+    },
+    {
+      link: "/",
+      text: "Take Out"
+    },
+  ]
+}
---
+++ frontend/src/theme/Theme.ts
@@ -0,0 +1,11 @@
+export const Theme = {
+  color: {
+    peach: "#FFEDDC",
+    yellow: "#FFC529"
+  },
+  font: {
+    family: "ABeeZee",
+    h1Size: "40px",
+    h2Size: "36px"
+  }
+}
