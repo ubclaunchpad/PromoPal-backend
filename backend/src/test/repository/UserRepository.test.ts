709a4fc6122b0aba0d1bf636eeb479e2309010d6
yunishimura0716 <yunishimura0716@gmail.com>
2020-11-17 22:55:05 -0800

    updated test for adding saved promotion

--- backend/src/test/repository/UserRepository.test.ts
+++ backend/src/test/repository/UserRepository.test.ts
@@ -1,6 +1,6 @@
 import { getConnection, getCustomRepository } from 'typeorm';
 import { User } from '../../main/entity/User';
-import { users_sample } from '../../main/resources/Data';
+import { promotions_sample, saved_promotions_mapping, users_sample } from '../../main/resources/Data';
 import { UserRepository } from '../../main/repository/UserRepository';
 import { BaseRepositoryTest } from './BaseRepositoryTest';
 
@@ -60,6 +60,34 @@ describe('Unit tests for UserRepository', function () {
       expect(e.detail).toEqual(
         `Key (password)=(${users_sample[0].password}) already exists.`
       );
+    }
+  });
+
+  test('Should be able to add promotion into saved promotion', async () => {
+    const before_add_user = await getConnection().createQueryBuilder().relation(User, "savedPromotions")
+                                .of(users_sample[2]).loadMany();
+    const before_add_saved = saved_promotions_mapping.get(users_sample[2])?.length; 
+    try {
+      await userRepository.addSavedPromotion(users_sample[2], promotions_sample[2]);
+    } catch(e) {
+      fail('Shouh have added');
+    }
+    const after_add_user = await getConnection().createQueryBuilder().relation(User, "savedPromotions")
+                                  .of(users_sample[2]).loadMany();
+    const after_add_saved = saved_promotions_mapping.get(users_sample[2])?.length;
+    expect(after_add_user.length).toEqual(before_add_user.length + 1);
+    if (before_add_saved) expect(after_add_saved).toEqual(before_add_saved + 1);
+  });
+  test('Should not be able to add promotion into saved promotion', async () => {
+    const before_add = await getConnection().createQueryBuilder().relation(User, "savedPromotions")
+                                .of(users_sample[2]).loadMany();
+    try {
+      await userRepository.addSavedPromotion(users_sample[2], promotions_sample[0]);
+      fail("Should have failed");
+    } catch(e) {
+      const savedpromotions = await getConnection().createQueryBuilder().relation(User, "savedPromotions")
+                                  .of(users_sample[2]).loadMany();
+      expect(savedpromotions.length).toEqual(before_add.length);
     }
   });
 });
