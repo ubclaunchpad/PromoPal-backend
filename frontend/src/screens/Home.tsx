83920bbf5c3086d7fc36b7a54dd1aa79ff08596b
Andrew Zulaybar <andrewzulaybar@gmail.com>
2020-11-18 20:09:54 -0800

    remove stub dropdowns and fix casing in import

--- frontend/src/screens/Home.tsx
+++ frontend/src/screens/Home.tsx
@@ -6,54 +6,6 @@ import PromotionList from "../components/PromotionList";
 import { usePromotionsList } from "../contexts/PromotionsListContext";
 import * as Promotion from "../types/promotion";
 
-import { DropdownOption, Dropdown} from "../types/Dropdown";
-
-const options: DropdownOption[] = [
-  {
-    link: "/link1",
-    text: "Option 1",
-  },
-  {
-    link: "/link2",
-    text: "Option 2",
-  },
-  {
-    link: "/link3",
-    text: "Option 3",
-  },
-];
-
-const dropdowns: Dropdown[] = [
-  {
-    text: "Sort",
-    options,
-  },
-  {
-    text: "Discount Type",
-    options,
-  },
-  {
-    text: "Cuisine",
-    options,
-  },
-  {
-    text: "Category",
-    options,
-  },
-  {
-    text: "Price",
-    options,
-  },
-  {
-    text: "Day of the Week",
-    options,
-  },
-  {
-    text: "Dine In Available",
-    options,
-  },
-];
-
 const mapWidth = 60;
 
 export default function Home(): ReactElement {
