c2667ac82ff0e544d6a2d5dad19cd72a27089800
yunishimura0716 <yunishimura0716@gmail.com>
2020-12-28 19:04:07 -0800

    modified test file for saved promotion using factory methods

--- backend/src/test/repository/SavedPromotion.test.ts
+++ backend/src/test/repository/SavedPromotion.test.ts
@@ -1,11 +1,16 @@
-import { getConnection, getCustomRepository, Connection } from 'typeorm';
-import { users_sample, promotions_sample } from '../../main/resources/Data';
+import { getCustomRepository} from 'typeorm';
 import connection from './BaseRepositoryTest';
 import { SavedPromotionRepository } from '../../main/repository/SavedPromotionRepository';
 import { UserRepository } from '../../main/repository/UserRepository';
 import { PromotionRepository } from '../../main/repository/PromotionRepository';
-import { User } from '../..//main/entity/User';
+import { User } from '../../main/entity/User';
 import { Promotion } from '../../main/entity/Promotion';
+import { UserFactory } from '../factory/UserFactory';
+import { PromotionFactory } from '../factory/PromotionFactory';
+import { Discount } from '../../main/entity/Discount';
+import { DiscountFactory } from '../factory/DiscountFactory';
+import { Schedule } from '../../main/entity/Schedule';
+import { ScheduleFactory } from '../factory/ScheduleFactory';
 
 describe('Unit tests for SavedPromotionRepository', function () {
   let savedPromotionRepository: SavedPromotionRepository;
@@ -27,51 +32,67 @@ describe('Unit tests for SavedPromotionRepository', function () {
   });
 
   test('Should be able to store a savedpromotion and successfully retrieve the saved promotion', async () => {
-    const user: User = users_sample[0];
-    const promotion: Promotion = promotions_sample[0];
+    // make a promotion
+    const user: User = new UserFactory().generate();
+    const discount: Discount = new DiscountFactory().generate();
+    const schedule: Schedule = new ScheduleFactory().generate();
+    const promotion: Promotion = new PromotionFactory().generate(user, discount, [schedule]);
     await userRepository.save(user);
     await promotionRepository.save(promotion);
-
-    await savedPromotionRepository.addSavedPromotion(user, promotion);
+    
+    // save the promotion
+    const saved_user: User = new UserFactory().generate();
+    const savedPromotion = savedPromotionRepository.create({userId: saved_user.id, promotionId: promotion.id});
+    await savedPromotionRepository.save(savedPromotion);
     const savedpromotion = await savedPromotionRepository.findOne({
-      user: user,
+      user: saved_user,
       promotion: promotion,
     });
     expect(savedpromotion).toBeDefined();
-    expect(savedpromotion!.userId).toEqual(user.id);
+    expect(savedpromotion!.userId).toEqual(saved_user.id);
     expect(savedpromotion!.promotionId).toEqual(promotion.id);
   });
 
   test('Should not able to store two same promotion for one user', async () => {
-    const user: User = users_sample[0];
-    const promotion: Promotion = promotions_sample[0];
+    // make a promotion
+    const user: User = new UserFactory().generate();
+    const discount: Discount = new DiscountFactory().generate();
+    const schedule: Schedule = new ScheduleFactory().generate();
+    const promotion: Promotion = new PromotionFactory().generate(user, discount, [schedule]);
     await userRepository.save(user);
     await promotionRepository.save(promotion);
-    await savedPromotionRepository.addSavedPromotion(user, promotion);
+    // save the promotion
+    const saved_user: User = new UserFactory().generate();
+    await savedPromotionRepository.addSavedPromotion(saved_user, promotion);
     try {
-      await savedPromotionRepository.addSavedPromotion(user, promotion);
+      await savedPromotionRepository.addSavedPromotion(saved_user, promotion);
       fail('Should have failed');
     } catch (e) {
       expect(e.detail).toEqual(
-        `Key (\"userId\", \"promotionId\")=(${user.id}, ${promotion.id}) already exists.`
+        `Key (\"userId\", \"promotionId\")=(${saved_user.id}, ${promotion.id}) already exists.`
       );
     }
   });
 
   test('Should be able to store a savedpromotion and successfully delete the saved promotion', async () => {
-    const user: User = users_sample[0];
-    const promotion: Promotion = promotions_sample[0];
+    // make a promotion
+    const user: User = new UserFactory().generate();
+    const discount: Discount = new DiscountFactory().generate();
+    const schedule: Schedule = new ScheduleFactory().generate();
+    const promotion: Promotion = new PromotionFactory().generate(user, discount, [schedule]);
     await userRepository.save(user);
     await promotionRepository.save(promotion);
-    await savedPromotionRepository.addSavedPromotion(user, promotion);
+    // save the promotion
+    const saved_user: User = new UserFactory().generate();
+    await savedPromotionRepository.addSavedPromotion(saved_user, promotion);
     try {
-      await savedPromotionRepository.deleteSavedPromotion(user, promotion);
+      await savedPromotionRepository.deleteSavedPromotion(saved_user, promotion);
     } catch (e) {
       fail('Should have succeeded to delete');
     }
     try {
       expect(await savedPromotionRepository.find({
-        user: user,
+        user: saved_user,
         promotion: promotion,
       })).toEqual([]);
     } catch (e) {
